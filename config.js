78ac149a22d8c457f63598d266fe57e21eb9dc41
sito8943 <sito8943@gmail.com>
2022-04-14 19:04:15 -0400

    init raw-server

---
+++ config.js
@@ -0,0 +1,37 @@
+// Import the functions you need from the SDKs you need
+const XMLHttpRequest = require("xhr2");
+var ab2str = require("arraybuffer-to-string");
+const { initializeApp } = require("firebase/app");
+const {
+  getStorage,
+  ref,
+  getDownloadURL,
+  getBytes,
+} = require("firebase/storage");
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBXVpiYBgZ1CwvM_EnloyHerEM0bpZ8sLA",
+  authDomain: "sito-server-docs.firebaseapp.com",
+  projectId: "sito-server-docs",
+  storageBucket: "sito-server-docs.appspot.com",
+  messagingSenderId: "989322908483",
+  appId: "1:989322908483:web:516a75ca838da4c44695f6",
+  measurementId: "G-4N5QL8E97V",
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+async function getFile(lang, file) {
+  // Get a reference to the storage service, which is used to create references in your storage bucket
+  const storage = getSt orage(firebaseApp);
+  const result = ab2str(await getBytes(ref(storage, `/${lang}/${file}.md`)));
+  return result;
+}
+
+module.exports = getFile;
+  
--- server.js
+++ server.js
@@ -1,20 +1,54 @@
-/* eslint-disable import/newline-after-import */
-/* eslint-disable prefer-arrow-callback */
 const express = require("express");
 const path = require("path");
-const fs = require("fs");
-const app = express();
+const cors = require("cors");
+const basicAuth = require("express-basic-auth");
 
-app.use(express.static(path.join(__dirname, "build")));
+// firebase
+const getFile = require("./config");
+
+const { docs } = require("./users/users");
+const { unauthorizedResponse } = require("./users/functions");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.set("etag", "strong"); //browser caching of static assets should work properly
+app.use(cors());
+app.options(
+  "/file",
+  cors({ origin: "http://localhost:3000", methods: "POST", credentials: true })
+); // include before other routes
+app.use(express.json({ limit: 1048576 }));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+// allow cross-origin requests
+/*app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});*/
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-app.post("/file", (req, res) => {
-  const { lang, file } = req.body.markdown.split(":");
-  const markdown = fs.readFileSync(`./files/${lang}/${file}`);
-  res.send(markdown);
+const router = express.Router();
+
+router.use(basicAuth({ users: docs, unauthorizedResponse }));
+
+router.post("/", async (req, res) => {
+  const splitted = req.body.markdown.Markdowns.split(":");
+  const markdown = await getFile(splitted[0], splitted[1]);
+  console.log(`${splitted[0]}/${splitted[1]}`);
+  res.send({ markdown });
 });
 
-app.listen(9000);
+app.use("/file", router);
+
+app.listen(port, () => {
+  console.log(`Sito docs server running ${port}`);
+});
